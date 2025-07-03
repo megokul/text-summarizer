815f7c648a245bbc43ad2bde8d97df0d01a31990
Gokul Krishna <iamgokul93@gmail.com>
2025-07-03 17:43:02 +0100

    data

---
+++ debug.py
@@ -0,0 +1,13 @@
+import transformers
+import sentencepiece
+import datasets
+import sacrebleu
+import rouge_score
+import py7zr
+
+print("transformers:", transformers.__version__)
+print("sentencepiece:", sentencepiece.__version__)
+print("datasets:", datasets.__version__)
+print("sacrebleu:", sacrebleu.__version__)
+print("rouge_score:", rouge_score.__version__)
+print("py7zr:", py7zr.__version__)
